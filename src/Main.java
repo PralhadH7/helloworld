3086426b27285158fdc4f4bbf495522b647c182d
Pralhad <pralhad.humagain77@gmail.com>
2025-04-15 12:06:59 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("Welcome to Java!");
+        System.out.println("This is a new commit!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
